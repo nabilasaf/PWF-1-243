36fef3cbaecbd3be0edf5faa0e5bee4dd6517197
nabilasaf <nabilasafitri861@gmail.com>
2026-03-02 05:27:10 +0700

    Pertemuan_Kedua

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+#Pertemuan Pertama
 Tampilan Web
 ![Tamplan Web](foto/image.png)
+
+#Pertemuan Kedua
+Tampilan /about.
+![Tampilan Web](foto/DashBoard.png)
+
+Register
+![Tampilan Web](foto/Regist.png)
+
+Login
+![Tampilan Web](foto/Login.png)
---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            About Me
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white p-6 shadow rounded">
+                <p><strong>Nama:</strong> Nabila Afifah Zalfaa Safitri</p>
+                <p><strong>NIM:</strong> 20230140243</p>
+                <p><strong>Program Studi:</strong> Informatika</p>
+                <p><strong>Hobi:</strong> Coding, Design, Nonton</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
